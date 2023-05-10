7aff6f75e78f19a2980198433b3505b5efbdf45f
miss <ddc.akari@gmail.com>
2023-05-11 03:56:04 +0900

    Context 설정

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import Header from "./components/Header";
 import Prototypes from "./components/Prototypes";
 import Orders from "./components/Orders";
 import Footer from "./components/Footer";
+import AppStateProvider from "./providers/AppStateProvider";
 
 function App() {
   return (
-    <>
-      <Header></Header>
+    <AppStateProvider>
+      <Header />
       <div className="container">
-        <Prototypes></Prototypes>
-        <Orders></Orders>
-        <Footer></Footer>
+        <Prototypes />
+        <Orders />
+        <Footer />
       </div>
-    </>
+    </AppStateProvider>
   );
 }
 
